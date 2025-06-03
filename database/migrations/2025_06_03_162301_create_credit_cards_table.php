87e40ea8fbe111773defdedc9b4aecabad61ebea
Md Ariful Islam <arifulatwork@gmail.com>
2025-06-03 19:25:26 +0300

    Update

---
+++ database/migrations/2025_06_03_162301_create_credit_cards_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('credit_cards', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('last4');
+            $table->string('expiry'); // MM/YY
+            $table->enum('type', ['visa', 'mastercard']);
+            $table->boolean('is_default')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('credit_cards');
+    }
+};
